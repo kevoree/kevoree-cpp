84761bd6db9e962c7cd64dfc99561331ad99aa44
JeD <jedartois@gmail.com>
2013-11-14 17:03:26 +0100

    add model generation

--- kevoree-core/model/clean.sh
+++ kevoree-core/model/clean.sh
@@ -1,6 +1,14 @@
+#!/bin/sh
+
+
+find . -name "CMakeFiles" | xargs rm -rf
+find . -name "Makefile" | xargs rm -rf
+find . -name "cmake_install.cmake_modules" | xargs rm -rf
+find . -name "cmake_install.cmake" | xargs rm -rf
+find . -name "CMakeCache.txt" | xargs rm -rf                                                
 rm -rf kevoree 
 rm -rf microframework
 rm -rf sparsehash
-rm check_leak.sh 
-rm clean_cmake_files.sh 
-rm generation.log
+rm -rf check_leak.sh 
+rm -rf clean_cmake_files.sh 
+rm -rf generation.log
--- kevoree-core/model/generate.sh
+++ kevoree-core/model/generate.sh
@@ -1,9 +1,7 @@
-
-echo "Cleaning previous generation"
 ./clean.sh
 echo "Generating Model"
 wget  https://oss.sonatype.org/content/repositories/snapshots/org/kevoree/modeling/org.kevoree.modeling.cpp.generator/1.9.1-SNAPSHOT/org.kevoree.modeling.cpp.generator-1.9.1-20131114.095000-5.jar --quiet
-java -jar org.kevoree.modeling.cpp.generator-1.9.1-20131114.095000-5.jar -i metamodel/kevoree.ecore -t . > generation.log
+java -jar org.kevoree.modeling.cpp.generator-1.9.1-20131114.095000-5.jar -i metamodel/kevoree.ecore -t .
 rm -rf META-INF
 rm -rf *.jar
-rm compile.sh
+rm -rf compile.sh
