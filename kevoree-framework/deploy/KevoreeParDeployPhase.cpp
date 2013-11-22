b81fddb208ff21c39a5ffdb1c0ae3ccfa97eb915
JeD <jedartois@gmail.com>
2013-11-22 18:29:30 +0100

    add KevoreeDeploy Phase and Primitive Command Execution helper

---
+++ kevoree-framework/deploy/KevoreeParDeployPhase.cpp
@@ -0,0 +1,13 @@
+#include <kevoree-framework/deploy/KevoreeParDeployPhase.h>
+
+
+
+KevoreeParDeployPhase::KevoreeParDeployPhase(){
+	maxTimeout=30000;
+}
+
+
+void KevoreeParDeployPhase::setMaxTime(long mt)
+{
+		maxTimeout=mt;
+}
