f3c2918ea074a577715bf607c75c1a0d9e5b09d0
JeD <jedartois@gmail.com>
2013-11-25 18:45:00 +0100

    add first comment for test purpose

---
+++ kevoree-library/HelloWorldComponent/src/HelloWorldComponent.cpp
@@ -0,0 +1,20 @@
+#include "HelloWorldComponent.h"
+
+
+// todo generate
+extern "C"{  
+    AbstractComponent* create(){
+    return new HelloWorldComponent();
+   }                        
+}
+extern "C" void destroy_object( AbstractComponent* object )
+{
+  delete object;
+}
+
+
+
+HelloWorldComponent::HelloWorldComponent()
+{
+	cout <<"HelloWorldComponent loaded" << endl;
+}
---
+++ kevoree-library/HelloWorldComponent/src/HelloWorldComponent.h
@@ -0,0 +1,12 @@
+#ifndef __HelloWorldComponent_H
+#define __HelloWorldComponent_H
+
+#include <kevoree-core/core/api/AbstractComponent.h>
+
+class HelloWorldComponent :public AbstractComponent
+{
+public:
+HelloWorldComponent();
+};
+
+#endif /*__HelloWorldComponent_H*/
