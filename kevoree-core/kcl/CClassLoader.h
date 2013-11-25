bea04cccf7afe936702369fa87d87c0717d6ee96
JeD <jedartois@gmail.com>
2013-11-25 18:43:32 +0100

    initial commit class loader

---
+++ kevoree-core/kcl/CClassLoader.h
@@ -0,0 +1,34 @@
+#ifndef CCLASSLOADER_H_INCLUDED
+#define CCLASSLOADER_H_INCLUDED
+
+
+#include <map>
+#include <string>
+
+using namespace std;
+
+
+class IClassLoader
+{
+public:
+	virtual void*				createClass() = 0;
+};
+
+
+class CClassLibrary
+{
+public:
+
+	static CClassLibrary*		getInstance();
+	CClassLibrary*				registerClassLoader(const string& in_className, IClassLoader* in_pNodeLoader);
+	void*						createClassByName(const string& in_className);
+
+private:
+	static CClassLibrary*		s_pInstance;
+	CClassLibrary() {}
+	virtual ~CClassLibrary() {}
+	map<string, IClassLoader*>	m_classLoadersByName;
+};
+
+
+#endif
