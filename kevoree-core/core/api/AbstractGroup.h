620eedbe4a94a776d5a4ab5763eefbef750bb8f1
JeD <jedartois@gmail.com>
2013-11-26 18:37:50 +0100

    add abstractgroup

--- kevoree-core/core/api/AbstractGroup.h
+++ kevoree-core/core/api/AbstractGroup.h
@@ -3,23 +3,39 @@
 
 
 #include <kevoree-core/model/kevoree/Group.h>
-
+#include <kevoree-core/model/kevoree/ContainerRoot.h>
 #include <string>
 
 
-class AbstractGroup
+class AbstractGroup  
 {
 public:
 	AbstractGroup()
 	{
 		instance=NULL;	
 	}
+	
+	virtual void start(){};
+	virtual void stop(){};
+	virtual void update(){};
+	
+	virtual bool push(ContainerRoot *root){};
+	virtual ContainerRoot* pull(std::string nodeName){};
+
+
+	
+	void update_model(ContainerRoot *model)
+	{
+		// todo 
+		
+	}
+
 
 	void setModelElement(Group *instance)
 	{
 		this->instance = instance;
 	}
-
+	
 private:
 Group *instance;
 
