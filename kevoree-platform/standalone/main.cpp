431fe8cf281ddc8520695cc1b46c6ba58aa17072
JeD <jedartois@gmail.com>
2013-12-05 16:21:44 +0100

    fix add deployunits containerroot

--- kevoree-platform/standalone/main.cpp
+++ kevoree-platform/standalone/main.cpp
@@ -14,10 +14,25 @@
 	#include <microframework/api/KMFContainer.h>
 	#include <microframework/api/utils/Logger.h>
 
-
+#include <kevoree-tools/DynamicLoader/DynamicLoader.h>
 	
 	int main(int argc,char **argv)
 	{
+		/*
+		DynamicLoader *t  = new DynamicLoader(NULL);
+		while(1)
+		{
+			void *handler =		t->soloader_load("build/libhelloworld_component.so");
+			AbstractTypeDefinition *type =	t->newInstance(handler);
+			type->start();
+			type->stop();
+			t->destroyInstance(handler,type);
+			
+			
+		}
+		*/
+		
+		
 		int exit=0;
 		clock_t start = clock();
 		LOGGER_START(Logger::DEBUG, "kevoree.log");
@@ -41,7 +56,7 @@
 			
 			TypeDefinition *nodetype = factory.createNodeType();
 			nodetype->name = "CPPNode";
-			nodetype->adddeployUnits(d);
+			nodetype->adddeployUnit(d);
 			
 					
 			DeployUnit *dg =factory.createDeployUnit();
@@ -51,37 +66,65 @@
 			
 			TypeDefinition *grouptype = factory.createGroupType();
 			grouptype->name ="WebSocketGroup";
-			grouptype->adddeployUnits(dg);
+			
+			DictionaryType *typegroup= factory.createDictionaryType();
+			DictionaryAttribute *attport = factory.createDictionaryAttribute();
+			attport->optional = false;
+			attport->datatype = "int";
+			attport->fragmentDependant = true;
+			attport->name ="port";
+			attport->defaultValue = "9000";
+			typegroup->addattributes(attport);
+			
+			grouptype->dictionaryType =typegroup;
+			
+			grouptype->adddeployUnit(dg);
 
 			
 			Group *group = factory.createGroup();
 			group->name ="group0";
 			group->started = true;
 			group->addtypeDefinition(grouptype);
-		
-
+			
+			Dictionary *dico =factory.createDictionary();
+			DictionaryValue *valport = factory.createDictionaryValue();
+			valport->name ="port";
+			valport->value ="9000";
+			
+			
+			dico->addvalues(valport);
+			group->dictionary=dico;
 			
 			TypeDefinition *comtype  = factory.createComponentType();
+			comtype->name = "comp";
 			
 			DeployUnit *dc =factory.createDeployUnit();
 			dc->name = "HelloWorldComponent";
 			dc->groupName = "org.kevoree.library";
 			dc->version = "1.0";
-			comtype->adddeployUnits(dc);
+			comtype->adddeployUnit(dc);
 			
 			ComponentInstance *c2 = factory.createComponentInstance();
 			c2->name ="HelloWorldComponent0";
-			c2->addtypeDefinition(comtype);
+
 
 			node0->addtypeDefinition(nodetype);
-			node0->addgroups(group);
+
 			node0->addcomponents(c2);
 
 			model->addtypeDefinitions(grouptype);
 			model->addtypeDefinitions(nodetype);
 			model->addtypeDefinitions(comtype);
+			model->adddeployUnits(d);
+			model->adddeployUnits(dg);
+			model->adddeployUnits(dc);
+			
+			c2->addtypeDefinition(comtype);
 			model->addnodes(node0);
+			
 			model->addgroups(group);
+			group->addsubNodes(node0);
+			node0->addgroups(group);
 
 
 			kb->setBootstrapModel(model); // boostrapmodel
