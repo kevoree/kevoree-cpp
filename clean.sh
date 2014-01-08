dae0950bace302ecde1c67b525e001e61cb9b423
JeD <jedartois@gmail.com>
2014-01-08 17:02:00 +0100

    add model tests

--- clean.sh
+++ clean.sh
@@ -6,8 +6,7 @@ find . -name "Makefile" | xargs rm -rf
 find . -name "cmake_install.cmake_modules" | xargs rm -rf
 find . -name "cmake_install.cmake" | xargs rm -rf
 find . -name "CMakeCache.txt" | xargs rm -rf
-find . -name "*.a" | xargs rm -rf                                      
-find . -name "*.json" | xargs rm -rf           
+find . -name "*.a" | xargs rm -rf                                            
 find . -name "*.so" | xargs rm -rf           
 rm -rf thirdparty/
 #rm -rf maven-resolver-cpp/
