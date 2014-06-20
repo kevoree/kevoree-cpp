fc9b226a72406a7a856c7ab1457c48801dcd6c66
jed <jedartois@gmail.com>
2014-06-20 16:22:10 +0200

    add platform runner option to specify nodename and model

--- kevoree-platform/standalone/main.cpp
+++ kevoree-platform/standalone/main.cpp
@@ -11,14 +11,153 @@
 
 #include <kevoree-tools/DynamicLoader/DynamicLoader.h>
 
+#include <string>
+#include <boost/filesystem.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+namespace po = boost::program_options;
+
+DefaultkevoreeFactory factory;
+JSONModelLoader loader;
 ContainerRoot *model=NULL;
 KevoreeBootStrap *kb=NULL;
 
-void my_handler(int s)
+/* configuration */
+const string log_file ="kevoree.log";
+const std::string name = "Kevoree Platform";
+const std::string version = "BETA 1.0.0";
+std::string path_bootstrap_model = "";
+std::string nodename = "node0";
+
+void sig_handler(int s);
+
+void defaultbootstrapmodel(std::string nodename);
+
+
+int main (int argc, char *argv[])
+{
+
+	Logger::Start(Logger::INFO, log_file);
+
+	po::options_description description(name+" "+version+"Usage");
+
+	description.add_options()
+			("help,h", "Display this help message")
+			("nodeName,n",po::value<std::string>(&nodename), " nodeName")
+			("version,v", "Display the version number")
+			("model,m", po::value<std::string>(),"bootstrap model path")
+			("level,l", po::value<int>(),"debug level");
+
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, description), vm);
+	po::notify(vm);
+
+	if(vm.count("help")){
+		std::cout << description;
+
+		return 0;
+	}
+
+	if(vm.count("version")){
+		std::cout << name+" "+version << std::endl;
+
+		return 0;
+	}
+	if(vm.count("level")){
+
+		int level = vm["level"].as<int>();
+		switch(level){
+		case 0:
+			Logger::Start(Logger::DEBUG, log_file);
+			break;
+
+		case 1:
+			Logger::Start(Logger::INFO, log_file);
+			break;
+
+		case 2:
+			Logger::Start(Logger::ERROR, log_file);
+			break;
+		}
+
+	}
+
+	if(vm.count("nodeName")){
+		nodename = vm["nodeName"].as<std::string>();
+	}
+
+	if(vm.count("model")){
+
+		path_bootstrap_model = vm["model"].as<std::string>();
+		Logger::Write(Logger::DEBUG, "Reading model");
+		ifstream modelfile;
+		modelfile.open(path_bootstrap_model);
+		std::string json_model;
+		string buffer;
+		while(!modelfile.eof()) // To get you all the lines.
+		{
+			getline(modelfile,buffer); // Saves the line in STRING.
+			json_model += buffer;
+		}
+		modelfile.close();
+		vector<KMFContainer*> *models =loader.loadModelFromString(json_model);
+		if(models != NULL && !models->empty())
+		{
+
+			model  =	(ContainerRoot*)models->front();
+
+		}else
+		{
+
+			return 0;
+		}
+
+
+	}
+
+
+
+	if(model == NULL)
+	{
+		defaultbootstrapmodel(nodename);
+	}
+
+
+	signal (SIGINT,sig_handler);
+	int exit=0;
+	clock_t start = clock();
+
+	kb = new KevoreeBootStrap();
+
+	kb->setNodeName(nodename);
+
+	boost::filesystem::path temp = boost::filesystem::temp_directory_path();
+	const std::string tempstr    = temp.native()+"/.m2";
+	kb->setBasePath(tempstr);
+
+	loader.setFactory(&factory);
+
+
+
+	kb->setBootstrapModel(model); // boostrapmodel
+	kb->start();
+
+
+
+	clock_t finish = clock();
+	std::cin >> exit;
+}
+
+
+
+void sig_handler(int s)
 {
 
 	if(kb !=NULL){
-		//kb->stop();	
+		//kb->stop();
 		delete kb;
 	}
 	if(model != NULL)
@@ -29,27 +168,8 @@ void my_handler(int s)
 
 
 
-int main(int argc,char **argv)
-{
-	Logger::Start(Logger::DEBUG, "kevoree.log");
+void defaultbootstrapmodel(std::string nodename){
 
-	DefaultkevoreeFactory factory;
-	JSONModelLoader loader;
-
-	signal (SIGINT,my_handler);
-	int exit=0;
-	clock_t start = clock();
-
-	kb = new KevoreeBootStrap();
-
-	// FIX ME PARAM
-	kb->setNodeName("node0");
-	kb->setBasePath("/tmp/.m2"); // FIX ME $HOME/.m2
-
-
-
-
-	loader.setFactory(&factory);
 
 	model = factory.createContainerRoot();
 
@@ -95,7 +215,7 @@ int main(int argc,char **argv)
 
 
 	ContainerNode *node0 = factory.createContainerNode();
-	node0->name = "node0";
+	node0->name = nodename;
 	node0->started= true;
 
 
@@ -131,7 +251,7 @@ int main(int argc,char **argv)
 	node0->addtypeDefinition(nodetype);
 
 
-	/* Temperature */
+	/* Temperature
 	TypeDefinition *helloworldtype  = factory.createComponentType();
 	helloworldtype->name = "HelloWorldComponent10101";
 	helloworldtype->abstract = false;
@@ -148,8 +268,8 @@ int main(int argc,char **argv)
 
 
 	helloworldtype->adddeployUnit(dcano);
-
-	model->addtypeDefinitions(helloworldtype);
+	 */
+	//	model->addtypeDefinitions(helloworldtype);
 	model->addtypeDefinitions(grouptype);
 	model->addtypeDefinitions(nodetype);
 
@@ -157,7 +277,7 @@ int main(int argc,char **argv)
 	model->adddeployUnits(d);
 	model->adddeployUnits(dg);
 	model->adddeployUnits(dc);
-	model->adddeployUnits(dcano);
+	//	model->adddeployUnits(dcano);
 
 
 	model->addnodes(node0);
@@ -174,21 +294,7 @@ int main(int argc,char **argv)
 
 	model->addrepositories(repo);
 
-	kb->setBootstrapModel(model); // boostrapmodel
-	kb->start();
-
-
-
-	clock_t finish = clock();
-	std::cin >> exit;
 }
-
-
-
-
-
-
-
 
 
 
