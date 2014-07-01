2648e50ebc10a232b283fe43e696dcc66cf9b046
jed <jedartois@gmail.com>
2014-07-01 11:35:28 +0200

    update tools

---
+++ kevoree-tools/java/org.kevoree.library.cpp.preprocessor/src/main/java/org/kevoree/cpp/preprocessor/lexer/Token.java
@@ -0,0 +1,48 @@
+package org.kevoree.cpp.preprocessor.lexer;
+
+/**
+ * Created by jed on 30/06/14.
+ */
+
+
+public class Token {
+
+    public enum  TokenType {
+        PRAGMA,
+        SOURCE,
+        VALUE
+        ,COMMA,END_OF_FILE
+    };
+
+    TokenType type;
+    String id;
+    String value;
+
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public void setType(TokenType type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public String toString(){
+        return type+" "+ id+" "+value;
+    }
+}
---
+++ kevoree-tools/java/org.kevoree.library.cpp.preprocessor/src/main/resources/test/HelloWorld.h
@@ -0,0 +1,16 @@
+
+
+#pragma NodeType "CPPNode"
+#pragma GroupName "org.kevoree.library"
+#pragma Version "1.0"
+
+#pragma Dictionary "port"
+#pragma Dictionary "num_threads"
+class CPPNodeType
+{
+public:
+	CPPNodeType();
+	virtual ~CPPNodeType();
+
+
+};
