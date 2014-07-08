0a382f380adda84467693ef42a4e63c410f95d8c
jed <jedartois@gmail.com>
2014-07-08 11:33:14 +0200

    add waxeye test

---
+++ tests/src/tests/Waxeye.c
@@ -0,0 +1,79 @@
+
+#include "Waxeye.h"
+
+
+char* ReadFile(char *filename)
+{
+	char *buffer = NULL;
+	int string_size,read_size;
+	FILE *handler = fopen(filename,"r");
+
+	if (handler)
+	{
+		//seek the last byte of the file
+		fseek(handler,0,SEEK_END);
+		//offset from the first to the last byte, or in other words, filesize
+		string_size = ftell (handler);
+		printf("KevScript size %d octets \n",string_size);
+		//go back to the start of the file
+		rewind(handler);
+
+		//allocate a string that can hold it all
+		buffer = (char*) malloc (sizeof(char) * (string_size + 1) );
+		//read it all in one operation
+		read_size = fread(buffer,sizeof(char),string_size,handler);
+		//fread doesnt set it so put a \0 in the last position
+		//and buffer is now officialy a string
+		buffer[string_size] = '\0';
+
+		if (string_size != read_size) {
+			//something went wrong, throw away the memory and set
+			//the buffer to NULL
+			free(buffer);
+			buffer = NULL;
+		}
+	}else {
+
+		return NULL;
+	}
+
+	return buffer;
+}
+
+
+
+
+int test(){
+
+
+	// Create our parser
+	struct parser_t *parser = parser_new();
+
+	// Setup our input
+	char *data =	ReadFile("bigScript.kevs");
+	if(data){
+		struct input_t *input = input_new(data, strlen(data));
+
+		// Parse our input
+		struct ast_t *ast = parse(parser, input);
+		if(ast == NULL){
+			return -1;
+		}
+
+		// Print our ast
+		display_ast(ast, type_strings);
+
+
+		// delete
+		ast_recursive_delete(ast);
+		input_delete(input);
+		parser_delete(parser);
+
+	}else {
+		printf("KevScript file not found \n");
+		return -1;
+	}
+
+
+	return 0;
+}
---
+++ tests/src/tests/Waxeye.h
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <parser.h>
+#include <string.h>
+
+#ifndef KEVSCRIPTTEST_C__H_
+#define KEVSCRIPTTEST_C__H_
+
+
+char* ReadFile(char *filename);
+
+int test();
+
+
+#endif
