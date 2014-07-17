a9ba9f547a2218e29b642575aaf22d4ee8da2af5
jed <jedartois@gmail.com>
2014-07-17 15:54:28 +0200

    add channel api core

---
+++ kevoree-core/api/AbstractChannel.h
@@ -0,0 +1,17 @@
+#ifndef __AbstractGroup_H
+#define __AbstractGroup_H
+
+
+#include <kevoree-core/model/kevoree/Channel.h>
+#include <kevoree-core/model/kevoree/ContainerRoot.h>
+#include <kevoree-core/api/AbstractTypeDefinition.h>
+#include <string>
+
+
+class AbstractChannel  : public AbstractTypeDefinition
+{
+
+};
+
+
+#endif
