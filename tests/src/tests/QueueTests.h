20c137083e93432aaf85f3226a6f6ddc7d92f299
jed <jedartois@gmail.com>
2014-07-22 09:58:31 +0200

    d

--- tests/src/tests/QueueTests.h
+++ tests/src/tests/QueueTests.h
@@ -12,13 +12,13 @@
 
 #include <iostream>
 #include <boost/thread.hpp>
-
+/*
 #include <spsc-queue.h>
 #include <mpsc-queue.h>
 #include <spsc-bounded-queue.h>
 #include <mpmc-bounded-queue.h>
 
-
+*/
 
 class QueueTests : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE( QueueTests );
