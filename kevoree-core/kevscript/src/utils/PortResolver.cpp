f34b54fadd06d94372ce5b27a403807f70a8c2b5
Aymeric <aymeric.hervieu@gmail.com>
2014-07-30 16:56:44 +0200

    port dev

--- kevoree-core/kevscript/src/utils/PortResolver.cpp
+++ kevoree-core/kevscript/src/utils/PortResolver.cpp
@@ -19,9 +19,47 @@ extern "C" {
 
 list<Port*>* PortResolver::resolve(struct ast_t *ast, ContainerRoot *model){
 	list<Port*>* resolved = new list<Port*>() ;
+    struct vector_t *child = ast->data.tree->children;
+	if((ast->type == TYPE_INSTANCEPATH) && child->size < 3){
+		string nodeName = string(ast_children_as_string((struct ast_t*) vector_get(child,0)));
+		string componentname = string(ast_children_as_string((struct ast_t*) vector_get(child,1)));
+		string portName = string(ast_children_as_string((struct ast_t*) vector_get(child,2)));
 
+		list<ContainerNode*>* node_list = new list<ContainerNode*>() ;
+		std::map<string,ContainerNode*> nodeMap =	model->nodes ;
+		if(nodeName.compare("*"))
+		{
+			for(std::map<string,ContainerNode*>::iterator it = nodeMap.begin(); it != nodeMap.end() ; ++it)
+				{
+				node_list->push_back(it->second) ;
+				}
+		}else {
+			ContainerNode * cn = model->findnodesByID(nodeName) ;
+			if(cn != NULL)
+				{
+			node_list->push_back(cn);
+				}
+			}
+		if(node_list->empty()){
+			throw string("No nodes resolved from : " + componentname) ;
+		}
 
+		for(std::list<ContainerNode*>::iterator it = node_list->begin() ; it != node_list->end() ; ++it){
+				ContainerNode * cn = *it ;
+				list<ComponentInstance*>* cil = new list<ComponentInstance*>();
+				std::map<string,ComponentInstance*> CIMap = cn->components ;
+				if(componentname.compare("*")){
+				for(std::map<string,ComponentInstance*>::iterator itct = CIMap.begin(); itct != CIMap.end() ; ++itct){
+						ComponentInstance * ct = itct->second ;
+						cil->push_back(ct) ;
+					}
+				}else
+				{
+					cil->push_back(cn->findcomponentsByID(componentname));
+				}
+		}
 
+	}
 	return resolved ;
 
 }
