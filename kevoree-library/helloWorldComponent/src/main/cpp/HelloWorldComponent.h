253ab2ee9b5d10f4ac041712a32d6bcc0da3a81b
Aymeric <aymeric.hervieu@gmail.com>
2014-08-27 11:42:47 +0200

    refazctoring  + mvn compile

---
+++ kevoree-library/helloWorldComponent/src/main/cpp/HelloWorldComponent.h
@@ -0,0 +1,20 @@
+#ifndef __HelloWorldComponent_H
+#define __HelloWorldComponent_H
+
+#include <kevoree-core/api/AbstractComponent.h>
+
+
+#pragma ComponentType "HelloWorldComponent"
+class HelloWorldComponent :public AbstractComponent
+{
+	public:
+	HelloWorldComponent();
+	~HelloWorldComponent();
+
+	 void start();
+	 void stop();
+	 void update();
+
+};
+
+#endif /*__HelloWorldComponent_H*/
