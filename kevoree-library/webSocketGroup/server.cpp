248ee03f96c71ed5fddd435bff429334ae4adf80
Aymeric <aymeric.hervieu@gmail.com>
2014-09-03 16:29:00 +0200

    Addin WSGroup

---
+++ kevoree-library/webSocketGroup/server.cpp
@@ -0,0 +1,8 @@
+#include "src/WebSocketGroup.h"
+
+
+int main(int argc,char **argv)
+{
+	WebSocketGroup *t = new WebSocketGroup();
+	
+}
---
+++ kevoree-library/webSocketGroup/src/main/cpp/WebSocketGroup.h
@@ -0,0 +1,49 @@
+#ifndef __WebSocketGroup_H
+#define __WebSocketGroup_H
+
+#include <kevoree-core/api/AbstractGroup.h>
+#include <kevoree-core/model/kevoree/DefaultkevoreeFactory.h>
+#include <microframework/api/json/JSONModelSerializer.h>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <microframework/api/json/JSONModelLoader.h>
+#include <websocketpp/server.hpp>
+
+
+#include <iostream>
+#include <thread>
+
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+typedef websocketpp::lib::shared_ptr<websocketpp::lib::thread> thread_ptr;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+// pull out the type of messages sent by our config
+typedef server::message_ptr message_ptr;
+
+
+#pragma GroupType "kevoree-group-websocket"
+#pragma Param "port" defaultValue="9000" fragdep
+
+class WebSocketGroup :public AbstractGroup
+{
+public:
+	WebSocketGroup();
+    void start();
+	void stop();
+	void update();
+	
+JSONModelSerializer ser;
+JSONModelLoader loader;
+DefaultkevoreeFactory factory;
+std::vector<thread_ptr> ts;
+server group;
+
+
+size_t num_threads;
+
+};
+
+#endif /*__WebSocketGroup_H*/
